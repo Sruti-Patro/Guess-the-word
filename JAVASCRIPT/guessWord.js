288dc91b19e0bbb8da733201f83e915a9063ab6f
Sruti Patro <patrocorgi29@gmail.com>
2024-08-01 02:31:32 +0530

    initial setting

---
+++ JAVASCRIPT/guessWord.js
@@ -0,0 +1,68 @@
+
+let word;
+// guess div invisible 
+let actionOnGuessOnClickOfSet= document.getElementById("set");
+actionOnGuessOnClickOfSet.onclick = () => {
+  //store the guess word
+  const setWord = document.querySelector(".headAns").value;
+  console.log(setWord);
+  word=setWord;
+  if (setWord=="") {
+    alert("You ave not set a word which is to be guessed. Set a word.");
+  } else {
+    //set div invisible
+  document.querySelector(".head").classList.add("invisible");
+  //guess div is visible
+  document.querySelector(".guess").classList.remove("invisible");
+  // guess button visible
+  document.getElementById("guess").classList.remove("invisible");
+  //set button invisible 
+  document.getElementById("set").classList.add("invisible");
+    handleGuessWord();
+  }
+};
+
+// Function to handle guessing the word
+let count=false;
+const handleGuessWord = () => { 
+   let guessWord = document.querySelector(".guessAns").value;
+   console.log(`Guessed word is ${guessWord}`);
+   document.querySelector(".h1").innerHTML="Now Guess the Word.";
+   if (guessWord=="") {
+    if (!count) {
+      document.querySelector(".h1").innerHTML="Try to Guess the Word!";
+      count=!count;
+    } else {
+      document.querySelector(".h1").innerHTML=" Your input word is empty!!!";
+    }
+    // alert("Your input is blank. Enter a word.");
+   }
+   else if(guessWord.toLowerCase()==word.toLowerCase()){
+    document.querySelector(".h1").innerHTML="YOU WON!";
+    window.location.href = "won.html";
+   }
+   else if(guessWord!=word){
+    document.querySelector(".h1").innerHTML="Try Again!";
+    document.querySelector(".guessAns").value="";
+    }
+  };
+
+  // newMatch
+  document.querySelector(".newMatch").onclick=()=>{
+    window.location.reload();
+  }
+
+document.getElementById("guess").addEventListener("click", handleGuessWord);
+
+
+
+
+
+
+
+// set div invisible after the word is set
+// let actionOnSetOnClickOfGuess = document.getElementById("guess");
+// actionOnSetOnClickOfGuess.onclick = () => {
+//   // document.querySelector(".head").classList.add("invisible");
+  // document.querySelector(".guess").classList.remove("invisible");
+// };
